a3544726998688d594bf296a37fe27a091f4774c
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-09-13 10:35:17 -0300

    up

--- src/components/ui/cano1.tsx
+++ src/components/ui/cano1.tsx
@@ -4,11 +4,11 @@ export default function Cano1() {
     return(
         <Image 
             src="/topo-vazio_70_13zon.png"
-            width={100}
+            width={300}
             height={100}
-            style={{padding:4,zIndex:1 }}    
+            style={{zIndex:1,left:0, }}    
             alt="cano"
-            className="p-0 m-0"
+            className="left-0 sm:max-w-fit"
             priority />
     )
 };
