74b7ff38f28952bde9406ac5322be88ace0a0d3a
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-09-13 10:29:47 -0300

    colocando sombras nos links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,12 +4,12 @@ import Logo from "./ui/logo"
 export default function Navbar(){
     return(
         <>
-        <nav  className="bg-gradient-to-r from-golden via-semigolden to-green-400 to-green-500) shadow-3xl p-4 sticky top-0">
+        <nav  className="bg-gradient-to-r from-golden via-semigolden to-green-300 to-green-500) shadow-3xl p-4 sticky top-0">
             <div className="container mx-auto flex justify-between items-center">
                 <Cano1 />
                 <Logo />
-                <div className="hidden md:flex space-x-4">
-                    <a href="#" className="text-white">Home</a>
+                <div className="hidden md:flex space-x-4 [text-shadow:_0_2px_0_rgb(2_2_2_/_100%)]">
+                    <a href="#" className="text-white ">Home</a>
                     <a href="#" className="text-white">About</a>
                     <a href="#" className="text-white">Services</a>
                     <a href="#" className="text-white">Contact</a>
